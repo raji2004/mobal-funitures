68904f7da0245beb480744d2a6fe59784e773ebe
raji2004 <kingraj1344@gmail.com>
2022-11-03 23:38:12 +0100

    responsive fix

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ function App() {
 
   return (
     <div className="App">
+      
       <Home />
 
        <img src={reactLogo} className={'size'} alt="" />
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,125 +1,65 @@
 import Nav from "../../components/Nav/Nav";
-import { useState,useEffect } from "react";
-import chair from '../../assets/chair.png'
+import { useState, useEffect } from "react";
+import chair from "../../assets/chair.png";
 
-import star from '../../assets/star.svg'
-import imgdata from '../../components/img/img'
+import star from "../../assets/star.svg";
+import imgdata from "../../components/img/img";
 import "./Home.css";
 
 export default function Home() {
-  const[index,setindex]= useState(0)
-  const[img,setimg] = useState(imgdata[index])
+  const [index, setindex] = useState(0);
+  const [img, setimg] = useState(imgdata[index]);
 
   return (
-    <div>
+    <div className="max-width flex column">
       <Nav />
-        <div className="hero flex row padding spacebetween">
-            <div className="hero-info flex column">
-                <div className="h4">
-                    <h4>Modern and Classy Furniture</h4>
-                </div>
-                <div className="p">
-                    <p>Choose from handmade well-crafted quality wooden funiture</p>
-                </div>
-                <button className="btn">See More</button>
-            </div>
-            <div className="hero-img ">
-              <img src={chair} className="img"alt="t" />
-              <div className="shadow"></div>
-            </div>
+      <div className="hero flex row padding spacebetween">
+        <div className="hero-info flex column">
+          <div className="h4">
+            <h4>Modern and Classy Furniture</h4>
+          </div>
+          <div className="p">
+            <p>Choose from handmade well-crafted quality wooden funiture</p>
+          </div>
+          <button className="btn">See More</button>
         </div>
-        <div className="scroll flex row ">
-          <div className="container flex column ">
-            <div className="p zero"><p>Wood</p></div>
-            <div  className="img-div flex row spacebetween flexend" style={{backgroundImage:`url(${img})`}}>
-              <div className="star flex">
-                 <ul className="flex row zero">
-                  <li><img src={star} alt="s" /></li>
-                  <li><img src={star} alt="s" /></li>
-                  <li><img src={star} alt="s" /></li>
-                  <li><img src={star} alt="s" /></li>
-                  <li><img src={star} alt="s" /></li>
-                 </ul>
-              </div>
-              <button className="btn green">Customize</button>
-            </div>
+        <div className="hero-img flex column ">
+          <img src={chair} className="img" alt="t" />
+          <div className="shadow"></div>
+        </div>
+      </div>
+      <div className="scroll flex row ">
+        <div className="container flex column ">
+          <div className="p zero">
+            <p>Wood</p>
           </div>
-          <div className="container flex column ">
-            <div className="p zero"><p>Wood</p></div>
-            <div  className="img-div flex row spacebetween flexend" style={{backgroundImage:`url(${img})`}}>
-              <div className="star flex">
-                 <ul className="flex row zero">
-                  <li><img src={star} alt="s" /></li>
-                  <li><img src={star} alt="s" /></li>
-                  <li><img src={star} alt="s" /></li>
-                  <li><img src={star} alt="s" /></li>
-                  <li><img src={star} alt="s" /></li>
-                 </ul>
-              </div>
-              <button className="btn green">Customize</button>
+          <div
+            className="img-div flex row spacebetween flexend"
+            style={{ backgroundImage: `url(${img})` }}
+          >
+            <div className="star flex">
+              <ul className="flex row zero">
+                <li>
+                  <img src={star} alt="s" />
+                </li>
+                <li>
+                  <img src={star} alt="s" />
+                </li>
+                <li>
+                  <img src={star} alt="s" />
+                </li>
+                <li>
+                  <img src={star} alt="s" />
+                </li>
+                <li>
+                  <img src={star} alt="s" />
+                </li>
+              </ul>
             </div>
-          </div>
-          <div className="container flex column ">
-            <div className="p zero"><p>Wood</p></div>
-            <div  className="img-div flex row spacebetween flexend" style={{backgroundImage:`url(${img})`}}>
-              <div className="star flex">
-                 <ul className="flex row zero">
-                  <li><img src={star} alt="s" /></li>
-                  <li><img src={star} alt="s" /></li>
-                  <li><img src={star} alt="s" /></li>
-                  <li><img src={star} alt="s" /></li>
-                  <li><img src={star} alt="s" /></li>
-                 </ul>
-              </div>
-              <button className="btn green">Customize</button>
-            </div>
-          </div>
-          <div className="container flex column ">
-            <div className="p zero"><p>Wood</p></div>
-            <div  className="img-div flex row spacebetween flexend" style={{backgroundImage:`url(${img})`}}>
-              <div className="star flex">
-                 <ul className="flex row zero">
-                  <li><img src={star} alt="s" /></li>
-                  <li><img src={star} alt="s" /></li>
-                  <li><img src={star} alt="s" /></li>
-                  <li><img src={star} alt="s" /></li>
-                  <li><img src={star} alt="s" /></li>
-                 </ul>
-              </div>
-              <button className="btn green">Customize</button>
-            </div>
-          </div>
-          <div className="container flex column ">
-            <div className="p zero"><p>Wood</p></div>
-            <div  className="img-div flex row spacebetween flexend" style={{backgroundImage:`url(${img})`}}>
-              <div className="star flex">
-                 <ul className="flex row zero">
-                  <li><img src={star} alt="s" /></li>
-                  <li><img src={star} alt="s" /></li>
-                  <li><img src={star} alt="s" /></li>
-                  <li><img src={star} alt="s" /></li>
-                  <li><img src={star} alt="s" /></li>
-                 </ul>
-              </div>
-              <button className="btn green">Customize</button>
-            </div>
-          </div>
-          <div className="container flex column ">
-            <div className="p zero"><p>Wood</p></div>
-            <div  className="img-div flex row spacebetween flexend" style={{backgroundImage:`url(${img})`}}>
-              <div className="star flex">
-                 <ul className="flex row zero">
-                  <li><img src={star} alt="s" /></li>
-                  <li><img src={star} alt="s" /></li>
-                  <li><img src={star} alt="s" /></li>
-                  <li><img src={star} alt="s" /></li>
-                  <li><img src={star} alt="s" /></li>
-                 </ul>
-              </div>
-              <button className="btn green">Customize</button>
-            </div>
+            <button className="btn green">Customize</button>
           </div>
         </div>
+      </div>
     </div>
   );
 }
